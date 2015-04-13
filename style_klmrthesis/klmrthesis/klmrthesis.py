c18fea84a02cc8645d5ed0cdeffa8c81cedeab9c
Konrad Rudolph <konrad.rudolph@gmail.com>
2015-04-13 16:35:04 +0100

    Improve styling

--- style_klmrthesis/klmrthesis/klmrthesis.py
+++ style_klmrthesis/klmrthesis/klmrthesis.py
@@ -1,12 +1,19 @@
 # A Pygments style for my thesis.
 
 from pygments.style import Style
-from pygments.token import Keyword, Name, Comment, String, Error, Number, \
-        Operator, Whitespace
+from pygments.token import Text, Keyword, Name, Comment, String, Error, \
+        Number, Operator, Whitespace, Punctuation
 
 
 class KlmrThesisStyle(Style):
     default_styles = ''
     styles = {
-        Keyword: '#FF00FF'
+        Text: '#404040',
+        Name: '#404040',
+        Operator: '#404040',
+        Punctuation: '#404040',
+        Comment: '#808080',
+        Keyword: '#FF0000',
+        Number: '#429EB6',
+        String: '#DE8950'
     }
